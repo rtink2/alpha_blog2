d24ff7d04691a96a2d39481b3d50b9d4418e4b61
rtink2 <tinkham0408@gmail.com>
2022-08-12 23:02:55 -0400

    styled app

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates :title, presence: true, length: { min: 6, maximum: 100}
-    validates :description, presence: true, length: { min: 10, maximum: 1000}
+    validates :title, presence: true, length: {minimum: 3, maximum: 100}
+    validates :description, presence: true, length: { minimum: 10, maximum: 1000}
 end
